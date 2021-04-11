79477a351ac7ffe7f987e370d0fab82cf9f96a1a
Sárdi András <sardi.andras@gmail.com>
2021-04-11 16:38:40 +0200

    Create Pokemon.java

---
+++ Pokemon.java
@@ -0,0 +1,13 @@
+public class Pokemon {
+    private String nev;
+    private String allapot;
+
+    public Pokemon(String nev, String allapot) {
+        this.nev = nev;
+        this.allapot = allapot;
+        System.out.println("EGY POKEMON LETREJOTT");
+    }
+    public Pokemon(String nev) {
+        this(nev, "szabad");
+    }
+}
